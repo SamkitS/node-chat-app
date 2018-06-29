77d05495b2e4e16e0beaef8a2d8d1215eb0fc84f
SamkitS <samkitshah.nmims@gmail.com>
2018-06-29 14:43:41 +0530

    Send Location link in a new Tab and test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 it('should generate the correct message object', () => {
@@ -12,4 +12,16 @@ it('should generate the correct message object', () => {
     expect(message).toMatchObject({from,text});
 
 })
-})
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location message', () => {
+        var from = 'Samkit';
+        var latitude = 1;
+        var longitude = 1;
+        
+        var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+        expect(locationMessage.url).toBe(`https://www.google.com/maps?q=${latitude},${longitude}`);
+    });
+});
