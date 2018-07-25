10a1c67bdab08066de5ab921d2b98b1e982440f0
SamkitS <samkitshah.nmims@gmail.com>
2018-07-25 16:43:55 +0530

    Printing time stamps

--- public/js/index.js
+++ public/js/index.js
@@ -10,18 +10,20 @@ var socket = io();
     });
 
     socket.on('newMessage', function(message) {
+        var formattedTime = moment(message.createdAt).format('h:mm a');
         console.log('New Message', message);
         var li = jQuery('<li></li>');
-        li.text(`${message.from}: ${message.text}`);
+        li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
         jQuery('#messages').append(li);
     })
 
     socket.on ('newLocationMessage', function (message) {
+        var formattedTime = moment(message.createdAt).format('h:mm a');
         var li = jQuery('<li></li>');
         var a = jQuery('<a target ="_blank">My current Location</a>')
 
-        li.text(`${message.from}:`)
+        li.text(`Sent at ${formattedTime} ${message.from}: `)
         a.attr('href', message.url);
         li.append(a);
         jQuery('#messages').append(li);
