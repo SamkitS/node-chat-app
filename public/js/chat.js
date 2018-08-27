daae0468d46405bca0717e306a33d5098ba8166e
SamkitS <samkitshah.nmims@gmail.com>
2018-08-27 17:17:38 +0530

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,7 +77,6 @@ if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHei
        var messageTextbox = jQuery('[name=message]');
 
        socket.emit('createMessage', {
-           from: 'User',
            text: messageTextbox.val()
        }, function () {
             messageTextbox.val('')
